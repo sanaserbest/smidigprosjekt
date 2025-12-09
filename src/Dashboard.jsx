9ab052de4978322cd67ede5e803d2123152fd4ce
sanaserbest <151184931+sanaserbest@users.noreply.github.com>
2025-12-09 19:35:15 +0100

    Add files via upload

---
+++ src/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Dashboard.css";
+import Trend from "./Trend.jsx";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <Trend />
+    </div>
+  );
+};
+
+export default Dashboard;
