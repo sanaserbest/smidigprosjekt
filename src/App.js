620d6132c3123ec0fde4a0f49d7412c24b6cb7de
sihamhanoun <142783260+sihamhanoun@users.noreply.github.com>
2025-12-10 11:30:50 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import LoggInn from "./LoggInn";
+import KledaLogo from "./assets/KledaLogo.png";
 import Dashboard from "./Dashboard.jsx";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  if (!isLoggedIn) {
+    return <LoggInn onLoginSuccess={() => setIsLoggedIn(true)} />;
+  }
+
   return (
     <div className="App">
       <Dashboard />
